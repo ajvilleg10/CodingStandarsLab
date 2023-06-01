a656398bd6230d4590dcd03a35dea0282fa19270
ajvilleg10 <ajvilleg@espol.edu.ec>
2023-06-01 00:18:26 -0500

    Fixig some errors

--- maven/src/main/java/maven/Vacation.java
+++ maven/src/main/java/maven/Vacation.java
@@ -7,7 +7,7 @@ public class Vacation {
 	private int base_cost = 1000;
 	private double total_cost = 0;
 	
-
+	//Constructor of the class
 	public Vacation() {
 		
 	}
@@ -44,6 +44,7 @@ public class Vacation {
 		this.base_cost = base_cost;
 	}
 	
+	/* Return the total cost of the package and if the total is 0 return the value of -1 */
 	public double getTotal_cost() {
 		if (total_cost == 0){
 			return -1;
@@ -55,6 +56,8 @@ public class Vacation {
 		this.total_cost = total_cost;
 	}
 
+	/* Method responsible for verifying if the destination is a touristic 
+	post in order to add an additional cost */
 	public double verifyDestiny(String destination) {
 		int additional = 0;
 		if(destination.toUpperCase().equals("Paris")) {
@@ -66,6 +69,7 @@ public class Vacation {
 		return additional + this.base_cost;	
 	}
 	
+	/* Method that return the total cost of the package including all the restrictions */
 	public void getCost(double totalcost, int travelers, int duration) {
 		double discount = 0;
 		double fee = 200;
